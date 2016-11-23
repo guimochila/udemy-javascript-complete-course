3679c1f142645528bd8eb5c31a0b659c1dedf4e4
Guilherme Scaldelai <gui.mochila@gmail.com>
2016-11-23 22:19:25 +0000

    UIController: DOM Cache created for input elements. Public getinput and getDOMStrings methods created. Controller: Add init function and setup listeners.

--- 6-Budgety/src/js/app.js
+++ 6-Budgety/src/js/app.js
@@ -1,19 +1,53 @@
 // Budget Controller Module
 var budgetController = (function () {
-    
+
+ 
 
 })();
 
 // User-interface controller
 var UIController = (function () {
 
+    // DOM Cache
+    var DOMStrings = {
+        $inputType: document.querySelector('.add__type'),
+        $inputDescription: document.querySelector('.add__description'),
+        $inputValue: document.querySelector('.add__value'),
+        $inputBtn: document.querySelector('.add__btn')
+    };
+
+    return {
+        getinput: function () {
+            return {
+                type: DOMStrings.$inputType.value,
+                description: DOMStrings.$inputDescription.value,
+                value: DOMStrings.$inputValue.value
+            }
+        },
+        getDOMStrings: DOMStrings
+    }
+
 })();
 
-// App Controller
+// Global App Controller
 var controller = (function (budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function () {
+
+        var DOM = UICtrl.getDOMStrings;
+
+        DOM.$inputBtn.addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function () {
         // 1. Get input field data
+        var input = UICtrl.getinput();
 
         // 2. Add item to budget controller
 
@@ -22,17 +56,15 @@ var controller = (function (budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-
-        console.log('It works');
     };
-    
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('App started.');
+            setupEventListeners();
         }
-    });
+    }
 
 })(budgetController, UIController);
 
+controller.init();
