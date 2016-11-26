ac97f8bf38b17e7e4a1d8d46d262112e31d34fd6
Guilherme Scaldelai <gui.mochila@gmail.com>
2016-11-26 18:45:30 +0000

    budgetController: calculatePercentages() and getPercentage() added.

--- 6-Budgety/src/js/app.js
+++ 6-Budgety/src/js/app.js
@@ -7,13 +7,26 @@ var budgetController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function (totalIncome) {
+        if (totalIncome > 0){
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }    
+    };
+
+    Expense.prototype.getPercentage = function(){
+        return this.percentage;
     }
 
     var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
 
     var allExpensives = [];
     var allIncomes = [];
@@ -95,6 +108,19 @@ var budgetController = (function () {
             } else {
                 data.percentage = -1;
             }
+        },
+
+        calculatePercentages: function () {
+            data.allItems.exp.forEach(function (cur) {
+                cur.calcPercentage(data.total.inc);
+            });
+        },
+
+        getPercentage: function () {
+            var allPerc = data.allItems.exp.map(function (cur) {
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function () {
@@ -194,7 +220,7 @@ var UIController = (function () {
         getDOMStrings: function () {
             return DOMStrings;
         }
-    }
+    };
 
 })();
 
@@ -225,6 +251,16 @@ var controller = (function (budgetCtrl, UICtrl) {
         UICtrl.displayBudget(budget);
     };
 
+    var updatePercentages = function () {
+        // Calculate percentages
+        budgetCtrl.calculatePercentages();
+        // Read percentages from the budget controller
+        var percentages = budgetCtrl.getPercentage();
+        // Update the UI with the new percentages
+
+        console.log(percentages);
+    };
+
     var ctrlAddItem = function () {
         var input, newItem;
         // 1. Get input field data
@@ -242,6 +278,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculage and update percentages
+            updatePercentages();
         }
 
     };
@@ -265,6 +304,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
             // 3. Update and show the new budget
             updateBudget();
+
+            // 4. Calculage and update percentages
+            updatePercentages();
         }
     };
 
@@ -279,7 +321,7 @@ var controller = (function (budgetCtrl, UICtrl) {
             });
             setupEventListeners();
         }
-    }
+    };
 
 })(budgetController, UIController);
 
